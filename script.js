59fed28c972eddc5ab801dd765cc48685da6820c
dedisson-pro <juliotcarlossoho@gmail.com>
2026-02-24 03:51:44 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function afficherMessage() {
+    document.getElementById("info").textContent =
+    "Cette veille permet de comprendre les bases du numérique avant de développer des applications.";
+}
